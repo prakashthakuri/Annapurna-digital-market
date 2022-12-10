37f14041452416a2d17c9981296dad73a938fbea
Prakash Shahi Thakuri <prakashthakuri1994@gmail.com>
2022-12-10 12:57:28 -0600

    react router basics

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import Directory from "./components/directory/Directory.component";
+import {Routes, Route} from 'react-router-dom'
+import Home from "./routes/Home/Home.component";
 
 function App() {
 
-  const categories = [
-     {
-      id: 1,
-      title: 'Brass',
-      imageUrl: 'https://cdn.shoplightspeed.com/shops/640430/files/34875515/wyld-blue-home-antique-raw-brass-bowl.jpg'
-     },
-     {
-      id: 2,
-      title: 'Silver',
-      imageUrl: 'https://freepngimg.com/thumb/silver/28926-3-silver-transparent-image.png',
-     },     {
-      id: 3,
-      title: 'Gold',
-      imageUrl: 'https://freepngimg.com/thumb/money/70888-money-coin-coins-free-hq-image.png',
-     },     {
-      id: 4,
-      title: 'Copper',
-      imageUrl: ''
-     },
-     
 
-  ]
+  return(
 
-  return <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Home/>} />
+        
+    </Routes>
+
+
+  ) 
 }
 
 export default App;
---
+++ src/routes/Home/Home.component.jsx
@@ -0,0 +1,30 @@
+import Directory from "../../components/directory/Directory.component"
+
+export default function Home() {
+
+  const categories = [
+     {
+      id: 1,
+      title: 'Brass',
+      imageUrl: 'https://cdn.shoplightspeed.com/shops/640430/files/34875515/wyld-blue-home-antique-raw-brass-bowl.jpg'
+     },
+     {
+      id: 2,
+      title: 'Silver',
+      imageUrl: 'https://freepngimg.com/thumb/silver/28926-3-silver-transparent-image.png',
+     },     {
+      id: 3,
+      title: 'Gold',
+      imageUrl: 'https://freepngimg.com/thumb/money/70888-money-coin-coins-free-hq-image.png',
+     },     {
+      id: 4,
+      title: 'Copper',
+      imageUrl: ''
+     },
+     
+
+  ]
+
+  return <Directory categories={categories} />
+}
+
